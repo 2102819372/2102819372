0fc1f505450cea30089de20ecd6b1635406206a9
冷 晓飞 <2102819372@qq.com>
2023-11-28 05:32:29 +0000

    update

---
+++ nuxt_test/src/pages/charts/ch03/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import axios from "axios";
+import { useAsyncState } from "@vueuse/core";
+axios
+  .get("https://jsonplaceholder.typicode.com/todos/1")
+  .then((t) => console.log(t));
+</script>
+<template>
+  <div>ch03</div>
+</template>
--- nuxt_test/src/pages/index.vue
+++ nuxt_test/src/pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtLink to="/charts/ch01">去ch01</NuxtLink><br />
     <NuxtLink to="/charts/ch02">去ch02</NuxtLink>
+    <NuxtLink to="/charts/ch03">去ch03</NuxtLink>
   </div>
 </template>
